4732af5236f11abbab0cd6604278e9dcf9efff22
montanhadm <montanhadm@live.com>
2017-05-17 21:44:02 -0300

    v0.2.2a - Hotfix

--- create_db.py
+++ create_db.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('database/database.db')
+conn = sqlite3.connect('database/users.db')
 print("Database aberta com sucesso")
 
-conn.execute('CREATE TABLE leituras (DATA TEXT, HORA TEXT, VALOR INTEGER)')
-print("Tabela de leituras criada com sucesso")
+conn.execute('CREATE TABLE users (USER TEXT, PASS TEXT, GENDER TEXT)')
+print("Tabela de usuários criada com sucesso")
 conn.close()
